c68dca42b76e412570db1949c2b757fd83aa4f37
chvdileep <46498157+chvdileep@users.noreply.github.com>
2020-11-12 18:03:53 -0500

    pelican 2020

--- golf/src/index.js
+++ golf/src/index.js
@@ -87,7 +87,7 @@ mockFeed288 = () => {
 // read course data from a file
 courseFunc = () => {
     console.log("loading course config")
-    let fileName = 'LPGA-Drive-On-2020.json'
+    let fileName = 'Pelican-2020.json'
     let raw = fs.readFileSync(fileName, 'utf8')
     let someText = raw.replace(/(\r\n\t|\n|\r\t)/gm, "");
     let t = JSON.parse(someText)
